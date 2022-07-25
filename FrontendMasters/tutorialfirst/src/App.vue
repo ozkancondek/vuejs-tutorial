df9a3d70daf92c2fa6d2a9d44039bfc54451c8b7
ozkancondek <ozkncndek@gmail.com>
2022-07-25 14:05:41 +0200

    Folder structure created

---
+++ FrontendMasters/tutorialfirst/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <MyFirstComponent
+      header="Ozkan Condek"
+      paragraph="Here is small explanation about me"
+      myNum="7"
+    />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import MyFirstComponent from "./components/MyFirstComponent.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    MyFirstComponent,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  border: 2px solid red;
+  padding: 20px;
+}
+</style>
---
+++ FrontendMasters/tutorialfirst/src/components/MyFirstComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="first-component">
+    <h1>{{ header }}</h1>
+    <p>{{ paragraph }}</p>
+    <p>And here is my favorite number: {{ myNum }}</p>
+    <p>Follow me from <a href="https://github.com/ozkancondek/">here</a></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyFirstComponent",
+  props: {
+    header: String,
+    paragraph: String,
+    myNUm: Number,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+div {
+  border: 3px solid rgb(243, 78, 169);
+  margin-top: 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
