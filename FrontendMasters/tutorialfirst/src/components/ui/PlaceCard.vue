b3b0b739e93c9e45f98df57f7a071d54df00890f
ozkancondek <ozkncndek@gmail.com>
2022-08-10 13:10:00 +0200

    Props passing

---
+++ FrontendMasters/tutorialfirst/src/components/ui/PlaceCard.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="card">
+
+    <img  width="235" height="300" :src="img" alt="image">
+    <h2>{{name}}</h2>
+    <p>{{desc}}</p>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    }
+  },
+
+
+}
+</script>
+<style>
+.card{
+  display: flex;
+  justify-content: center;
+  align-items:center ;
+  flex-direction: column;
+  padding: 10px;
+}
+</style>
