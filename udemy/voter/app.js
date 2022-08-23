a62cef2fb7516269398968161f957ab455030061
ozkancondek <ozkncndek@gmail.com>
2022-08-23 09:38:58 +0200

    Event handling wit arguments and multiple event handler

--- udemy/voter/app.js
+++ udemy/voter/app.js
@@ -1,21 +1,25 @@
-
 //configuration
 const app = Vue.createApp({
 
-    data :function () {
+    data: function () {
         return {
-            submissions : submissions //from seed.js
+            submissions: submissions //from seed.js
         }
 
     },
-    methods : {
-      // increaseVote :function  () {
-      //     submissions[0].votes++
-      //
-      // }
-      //   dont use arrow function
-        upvote (){
+    methods: {
+        // increaseVote :function  () {
+        //     submissions[0].votes++
+        //
+        // }
+        //   dont use arrow function
+        upvote(consoleText,event) {
             this.submissions[0].votes++
+            // console.log(event)
+            // console.log(consoleText)
+        },
+        logConsole (text) {
+            console.log(text)
 
         }
     }
