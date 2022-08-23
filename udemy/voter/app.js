1c6f585eebcf74333f75eea1193692bdcfada7eb
ozkancondek <ozkncndek@gmail.com>
2022-08-23 10:19:39 +0200

    method, computed,watcher

--- udemy/voter/app.js
+++ udemy/voter/app.js
@@ -3,34 +3,59 @@ const app = Vue.createApp({
 
     data: function () {
         return {
-            submissions: submissions //from seed.js
+            submissions: submissions, //from seed.js
+            totalVotes: 0
         }
 
     },
-    computed : {
+    // computed : {
+    //
+    //     totalVotes (){
+    //         return  this.submissions.reduce((totalVotes,submission)=>{
+    //             return totalVotes + submission.votes
+    //         },0)
+    //     }
+    //
+    // },
 
-        totalVotes (){
-            return  this.submissions.reduce((totalVotes,submission)=>{
-                return totalVotes + submission.votes
-            },0)
-        }
-
-    },
     methods: {
         // increaseVote :function  () {
         //     submissions[0].votes++
         //
         // }
         //   dont use arrow function
-        upvote(consoleText,event) {
+        upvote(consoleText, event) {
             this.submissions[0].votes++
             // console.log(event)
             // console.log(consoleText)
         },
-        logConsole (text) {
+        logConsole(text) {
             console.log(text)
 
         },
+
+    },
+    watch: {//works for every possible change in data
+
+        // submissions(newValue, oldValue) {
+        //     console.log(newValue)
+        //     console.log(oldValue)
+        // },
+        submissions: { //this one is long version of function above
+            handler(newValue, oldValue) {
+                this.totalVotes = this.submissions.reduce((totalVotes, submission) => {
+                    return totalVotes + submission.votes
+                }, 0)
+
+            },
+            deep: true, //watching
+            immediate: true // take data when mounted. start with first value when its false
+        },
+        totalVotes(newValue,oldValue) {
+            console.log(oldValue)
+            console.log(newValue)
+        }
+
 
     }
 
