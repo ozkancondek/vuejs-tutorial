494edfb14b75a08b66085100124500f08e1305ff
ozkancondek <ozkncndek@gmail.com>
2022-08-23 23:10:01 +0200

    Compenents structure created

---
+++ udemy/termin-calendar/src/components/CalendarWeek.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="calender-week">
+    <div class="card-group">
+      <div class="card" v-for="i in 7" :key="i">
+        <div class="card-header text-center" role="button">
+          <strong>Montag</strong>
+        </div>
+        <div class="card-body">
+          <!-- Anfang: Template für die Calendar-Day-Component -->
+            <CalendarDay/>
+          <!-- Ende: Template für die Calendar-Day-Component -->
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import CalendarDay from "@/components/CalendarDay";
+export default {
+  name:"CalendarWeek",
+  components: {CalendarDay},
+}
+</script>
+<style scoped>
+
+
+
+</style>
