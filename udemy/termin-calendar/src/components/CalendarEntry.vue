2f7ca40c97999c7ca3d82ac93c147482e3387bc9
ozkancondek <ozkncndek@gmail.com>
2022-08-24 16:30:49 +0200

    Summit event button activate, disactivate

--- udemy/termin-calendar/src/components/CalendarEntry.vue
+++ udemy/termin-calendar/src/components/CalendarEntry.vue
@@ -1,23 +1,27 @@
 <template>
-<div id="calender-entry">
-  <div class="card">
-    <div class="card-header text-center">
-      <h5>Neuer Termin für: <strong>{{activeDayName}}</strong></h5>
-    </div>
-    <div class="card-body">
-<!--      {{event}}-->
-      <input
-          type="text"
-          class="form-control"
-          placeholder="Neuer Eintrag"
-          v-model="event.title"
-      />
-      <select class="form-select mt-2" v-model = "event.priority">
-        <option value="-1">Hoch</option>
-        <option value="0">Mittel</option>
-        <option value="1">Tief</option>
-      </select>
-      <div class="text-center mt-3">
+  <div id="calender-entry">
+    <div class="card">
+      <div class="card-header text-center">
+        <h5>Neuer Termin für: <strong>{{ activeDayName }}</strong></h5>
+      </div>
+      <div class="card-body">
+        <!--      {{event}}-->
+        <div class="alert alert-danger" v-if="error">
+          Der titel darf nicht leer sein!!!
+        </div>
+
+        <input
+            type="text"
+            class="form-control"
+            placeholder="Neuer Eintrag"
+            v-model="event.title"
+        />
+        <select class="form-select mt-2" v-model="event.priority">
+          <option value="-1">Hoch</option>
+          <option value="0">Mittel</option>
+          <option value="1">Tief</option>
+        </select>
+        <div class="text-center mt-3">
                 <span
                     v-for="color in eventColors"
                     :key="color"
@@ -27,49 +31,63 @@
                     @click="setEventColor(color)"
                 >
                 </span>
-      </div>
-      <hr />
-      <div class="d-grid gap-2">
-        <button class="btn btn-primary" @click = "submitEvent()">Eintragen</button>
-        <button class="btn btn-danger">Inhalt löschen</button>
+        </div>
+        <hr/>
+        <div class="d-grid gap-2">
+          <button class="btn btn-primary" @click="submitEvent()" :disabled="submitEventButtonStatus">Eintragen</button>
+          <button class="btn btn-danger">Inhalt löschen</button>
+        </div>
       </div>
     </div>
   </div>
-</div>
 </template>
 <script>
 import Store from "../store"
+
 export default {
-  name : "CalendarEntry",
-  data(){
-      return{
-        eventColors:["primary","success","info","warning","danger"],
-        event:{
-          title:"",
-          color:"primary",
-          priority:0
-        }
-      }
+  name: "CalendarEntry",
+  data() {
+    return {
+      eventColors: ["primary", "success", "info", "warning", "danger"],
+      event: {
+        title: "",
+        color: "primary",
+        priority: 0
+      },
+      error: false,
+    }
 
   },
-  props:{},
-  methods:{
-    eventColorClasses(eventColor){
-      return[ 'alert-' + eventColor,
-      this.event.color === eventColor ? "border border-" + this.event.color : ""
+  props: {},
+  methods: {
+    eventColorClasses(eventColor) {
+      return ['alert-' + eventColor,
+        this.event.color === eventColor ? "border border-" + this.event.color : ""
       ]
     },
-    setEventColor(eventColor){
+    setEventColor(eventColor) {
       this.event.color = eventColor;
 
     },
-    submitEvent(){
+    submitEvent() {
+      if (this.event.title === "") return (this.error = true)
       Store.mutations.storeEvent(this.event)
+      this.event = {
+        title: "",
+        color: "primary",
+        priority: 0
+      }
+      this.error = false
     }
   },
-  computed:{
+  computed: {
     activeDayName() {
-      return Store.getters.activeDay().fullName    }
+      return Store.getters.activeDay().fullName
+    },
+    submitEventButtonStatus (){
+      return this.event.title === ""
+
+    }
   }
 }
 </script>
