1e10e67ac2698e27c1cf978ed6fc12e2a04ca8ee
ozkancondek <ozkncndek@gmail.com>
2022-08-25 11:05:40 +0200

    Reactive from vue for unprimitive data types

--- udemy/comp-demo/src/components/HelloWorld.vue
+++ udemy/comp-demo/src/components/HelloWorld.vue
@@ -1,29 +1,49 @@
 <template>
   <div class="hello">
-    <h1>{{getGreeting}}</h1>
+    <h1>{{ getGreeting }}</h1>
     <br>
-    <button @click ="updateGreeting">Update</button>
+    <button @click="updateGreeting">Update primtive</button>
+    <br>
+    <hr>
+    <h1>{{ greetingObj.message }}-{{ greetingObj.desc }}</h1>
+    <br>
+    <button @click="updateGreetingObj">Update unprimitive</button>
 
 
   </div>
 </template>
 
 <script>
-//For primitive data types
-import {ref} from "vue"
+//For primitive data types ref()
+//for others reactive()
+import {ref, reactive} from "vue"
+
 export default {
   name: 'HelloWorld',
   //Wird ausgeführt bevor di Comüonent erzeugt wird
   //und nachdem die props verfügbar sind.
-  setup(){
+  setup() {
     //Logic,logic, logic
- const getGreeting = ref("Hello World")
-    const updateGreeting = ()=>{
-   return getGreeting.value= "Tex updated"
+    const getGreeting = ref("Hello World")
+    const updateGreeting = () => {
+      return getGreeting.value = "Tex updated"
     }
+    const greetingObj =reactive( {
+      message: "Salut",
+      desc: "Wilkommen zur App"
+    })
+    const updateGreetingObj = () => {
+
+      greetingObj.message = "Hallo"
+      greetingObj.desc = "Description updated"
+
+    }
+
     return {
       getGreeting,
-      updateGreeting
+      updateGreeting,
+      greetingObj,
+      updateGreetingObj
     }
   }
 
