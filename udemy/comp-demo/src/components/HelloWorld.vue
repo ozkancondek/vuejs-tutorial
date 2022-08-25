6fb43df183d8e946a7345f17810164e6c465493c
ozkancondek <ozkncndek@gmail.com>
2022-08-25 10:55:15 +0200

    NEw project:Composition Api => primitive data types with ref()

---
+++ udemy/comp-demo/src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    <h1>{{getGreeting}}</h1>
+    <br>
+    <button @click ="updateGreeting">Update</button>
+
+
+  </div>
+</template>
+
+<script>
+//For primitive data types
+import {ref} from "vue"
+export default {
+  name: 'HelloWorld',
+  //Wird ausgeführt bevor di Comüonent erzeugt wird
+  //und nachdem die props verfügbar sind.
+  setup(){
+    //Logic,logic, logic
+ const getGreeting = ref("Hello World")
+    const updateGreeting = ()=>{
+   return getGreeting.value= "Tex updated"
+    }
+    return {
+      getGreeting,
+      updateGreeting
+    }
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
