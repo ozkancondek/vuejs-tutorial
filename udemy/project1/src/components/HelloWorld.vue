022a7afc0991d99cba821058dcfe925eed783a64
ozkancondek <ozkncndek@gmail.com>
2022-08-29 16:07:09 +0200

    Project setup

--- udemy/project1/src/components/HelloWorld.vue
+++ udemy/project1/src/components/HelloWorld.vue
@@ -1,14 +1,27 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+
+    <p>{{myNum}}</p>
+
+    <p>{{myArr}}</p>
   </div>
 </template>
 
 <script>
+import {myArr} from "@/data"; //imported array£
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data(){
+   return {
+     myNum : myArr[0],  //value of myArr[0] equal to myNum
+     //another option is I can directly return myArr
+     myArr
+   }
   }
 }
 </script>
---
+++ udemy/project1/src/data.js
@@ -0,0 +1,1 @@
+export const myArr = [0,1,2]
